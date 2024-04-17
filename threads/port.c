d08c3f23a4d4b47ef4b9df1fb4eebc100eff902b
Mikhail Rybalko <147132802+MrBurzhuadotexe@users.noreply.github.com>
2024-04-17 05:33:11 +0200

    Create port.c

---
+++ threads/port.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define DOCKS 3
+#define TOWS 12
+
+pthread_mutex_t docks_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t tows_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t tow_available = PTHREAD_COND_INITIALIZER, dock_available = PTHREAD_COND_INITIALIZER;
+
+int free_docks = DOCKS;
+int free_tows = TOWS;
+
+void *ship_thread(void *m) {
+    int mass = *((int *) m);
+    pthread_mutex_lock(&docks_mutex);
+    if (free_docks == 0) {
+        pthread_cond_wait(&dock_available, &docks_mutex);
+    }
+    free_docks--;
+    pthread_mutex_unlock(&docks_mutex);
+
+    pthread_mutex_lock(&tows_mutex);
+    while (free_tows < mass) {
+        pthread_cond_wait(&tow_available, &tows_mutex);
+    }
+    free_tows -= mass;
+    printf("Ship with mass %d is being lifted %d %d\n", mass, free_tows, free_docks);
+    pthread_mutex_unlock(&tows_mutex);
+
+    sleep(2);
+
+    pthread_mutex_lock(&tows_mutex);
+    free_tows += mass;
+    pthread_cond_broadcast(&tow_available);
+    printf("Ship with mass %d docked. %d %d\n", mass, free_tows, free_docks);
+    pthread_mutex_unlock(&tows_mutex);
+
+    sleep(5);
+
+    pthread_mutex_lock(&tows_mutex);
+    while (free_tows < mass) {
+        pthread_cond_wait(&tow_available, &tows_mutex);
+    }
+    free_tows -= mass;
+    pthread_mutex_unlock(&tows_mutex);
+
+    pthread_mutex_lock(&docks_mutex);
+    free_docks++;
+    pthread_cond_signal(&dock_available);
+    printf("Ship with mass %d leaves the dock %d %d\n", mass, free_tows, free_docks);
+    pthread_mutex_unlock(&docks_mutex);
+
+    sleep(2);
+
+    pthread_mutex_lock(&tows_mutex);
+    free_tows += mass;
+    pthread_cond_broadcast(&tow_available);
+    printf("Ship with mass %d releases the cranes %d %d\n", mass, free_tows, free_docks);
+    pthread_mutex_unlock(&tows_mutex);
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t ships[argc];
+    for (int i = 0; i < argc; i++) {
+        int m = atoi(argv[i]);
+        if (pthread_create(&ships[i], NULL, ship_thread, &m) != 0) {
+            sleep(1);
+            perror("Creating thread");
+            exit(1);
+        }
+    }
+    for (int i = 0; i < argc; i++) {
+        pthread_join(ships[i], NULL);
+    }
+    return 0;
+}
